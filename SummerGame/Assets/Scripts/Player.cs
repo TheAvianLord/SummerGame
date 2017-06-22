3d4d19fe7c0c3fd385d37ce7da8a339e5de73c2b
TheAvianLord <angela-yixuan21@hotmail.com>
2017-06-21 17:22:31 -0700

    2D movement

---
+++ SummerGame/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D myRigidbody;
+    public float speed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        HandleMovement(horizontal);
+	}
+
+    private void HandleMovement(float horizontal)
+    {
+        myRigidbody.velocity = new Vector2(horizontal * speed, myRigidbody.velocity.y); 
+    }
+}
