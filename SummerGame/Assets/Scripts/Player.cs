b20bae521c48d24d6d5b6b7481e40015ac3ce3b0
TheAvianLord <angela-yixuan21@hotmail.com>
2017-06-21 23:13:49 -0700

    Player Flip

--- SummerGame/Assets/Scripts/Player.cs
+++ SummerGame/Assets/Scripts/Player.cs
@@ -6,10 +6,12 @@ public class Player : MonoBehaviour
 {
     private Rigidbody2D myRigidbody;
     public float speed;
+    public bool facingRight;
 
 	// Use this for initialization
 	void Start ()
     {
+        facingRight = true;
         myRigidbody = GetComponent<Rigidbody2D>();
 	}
 	
@@ -18,10 +20,23 @@ public class Player : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         HandleMovement(horizontal);
+        Flip(horizontal);
 	}
 
     private void HandleMovement(float horizontal)
     {
         myRigidbody.velocity = new Vector2(horizontal * speed, myRigidbody.velocity.y); 
+    }
+
+    private void Flip(float horizontal)
+    {
+        if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+            Vector3 theScale = transform.localScale;
+
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
     }
 }
