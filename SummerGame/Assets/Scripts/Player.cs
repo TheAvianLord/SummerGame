e392799b426909f7b364e0125cebc1b9b5ed589f
TheAvianLord <angela-yixuan21@hotmail.com>
2017-06-23 00:18:40 -0700

    Working on Jump

--- SummerGame/Assets/Scripts/Player.cs
+++ SummerGame/Assets/Scripts/Player.cs
@@ -5,27 +5,44 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D myRigidbody;
+    private Animator myAnimator;
     public float speed;
     public bool facingRight;
+    public Transform[] groundPoints;
+    public float groundRadius;
+    public LayerMask whatIsGround;
+    public bool isGrounded;
+    public bool jump;
+    public float jumpForce;
 
 	// Use this for initialization
 	void Start ()
     {
         facingRight = true;
         myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
         float horizontal = Input.GetAxis("Horizontal");
+        isGrounded = IsGrounded();
         HandleMovement(horizontal);
         Flip(horizontal);
 	}
 
     private void HandleMovement(float horizontal)
     {
-        myRigidbody.velocity = new Vector2(horizontal * speed, myRigidbody.velocity.y); 
+        myRigidbody.velocity = new Vector2(horizontal * speed, myRigidbody.velocity.y);
+        myAnimator.SetFloat("speed", Mathf.Abs(horizontal));
+
+        if (isGrounded && jump)
+        {
+            isGrounded = false;
+            myRigidbody.AddForce(new Vector2(0, jumpForce));
+        }
+
     }
 
     private void Flip(float horizontal)
@@ -38,5 +55,34 @@ public class Player : MonoBehaviour
             theScale.x *= -1;
             transform.localScale = theScale;
         }
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jump = true;
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        if (myRigidbody.velocity.y <= 0)
+        {
+            foreach (Transform point in groundPoints)
+            {
+                Collider2D[] colliders = Physics2D.OverlapCircleAll(point.position, groundPosition, whatIsGround);
+
+                for (int i = 0; i < colliders.Length; i++)
+                {
+                    if (colliders[i].gameObject != gameObject)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+        }
+        return false;
     }
 }
